d59cd9c1985816df3300a7eef03a8af0defaacf7
Asad Jafri <51427124+Asadhjafri@users.noreply.github.com>
2022-10-17 17:35:31 +0000

    typconvert age as an int. Add math logic to days remaining. Add F string result as a template.

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ age = input("What is your current age?")
 
 #Write your code below this line 👇
 
+age_as_int = int(age)
+years_remaining = 90 - age_as_int
+days_remaining = years_remaining * 3
+weeks_remaining = 
+months_remaining = 
 
+print(f"You have {days_remaining} days, {weeks_remaining} weeks, and a {months_remaining} months left.")
 
 
 
